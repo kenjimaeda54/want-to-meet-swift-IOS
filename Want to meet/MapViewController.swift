3d9be60023631b5d4e06ee91b71f1b7c10e5195f
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-08-19 08:20:47 -0300

    feat: finished app

--- Want to meet/MapViewController.swift
+++ Want to meet/MapViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 import MapKit
 
+enum MapMessageAlert {
+	case error
+	case authorizationWarning
+}
+
 class MapViewController: UIViewController {
 	
 	
@@ -20,6 +25,9 @@ class MapViewController: UIViewController {
 	
 	var places: [Place]!
 	var annotationPoi: [MKAnnotation] = []
+	var locationManager = CLLocationManager()
+	var buttonLocation = MKUserTrackingButton()
+	var selectedAnotation: PlaceAnnotation?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -27,6 +35,7 @@ class MapViewController: UIViewController {
 		//para customizar os pin
 		mapView.delegate = self
 		searchBar.delegate = self
+		locationManager.delegate = self
 		
 		//mudar cor do background color do textfiled do search bar
 		searchBar.searchTextField.backgroundColor = .white
@@ -43,6 +52,39 @@ class MapViewController: UIViewController {
 			addPin(it)
 		}
 		showPin()
+		requestLocationUser()
+		buttonLocationUser()
+		
+	}
+	
+	func buttonLocationUser() {
+		buttonLocation = MKUserTrackingButton(mapView: mapView)
+		buttonLocation.backgroundColor = .white
+		buttonLocation.frame.origin.x = 10
+		buttonLocation.frame.origin.y = 10
+		buttonLocation.layer.cornerRadius = 5
+		buttonLocation.layer.borderWidth = 1
+		buttonLocation.layer.borderColor = UIColor(named: "main")?.cgColor
+	}
+	
+	//aqui estou verificando os recursos de localizacao
+	func requestLocationUser() {
+		//lembra que precisa em Info.plist colocar uma mensagem que ira
+		//usar servicos de localizacao
+		if CLLocationManager.locationServicesEnabled() {
+			switch locationManager.authorizationStatus {
+			case .authorizedAlways,.authorizedWhenInUse:
+				mapView.addSubview(buttonLocation)
+			case .denied:
+				alertLocation(.authorizationWarning)
+			case .notDetermined:
+				locationManager.requestWhenInUseAuthorization()
+			default:
+				break
+			}
+			
+		}
+		
 	}
 	
 	//criando as anotacoes para pin
@@ -70,6 +112,56 @@ class MapViewController: UIViewController {
 	
 	
 	@IBAction func handleTraceRote(_ sender: UIButton) {
+		if  locationManager.authorizationStatus != .authorizedWhenInUse {
+			alertLocation(.authorizationWarning)
+			return
+		}
+		//destionation pegando a localizacao do ponto de interesse
+		if let destinationSelected = selectedAnotation?.coordinate,
+			 // em locationManager estou pegando a localizacao do usuario
+			 let sourceSelected = locationManager.location?.coordinate {
+			
+			let destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationSelected))
+			let source = MKMapItem(placemark: MKPlacemark(coordinate: sourceSelected))
+			
+			let request = MKDirections.Request()
+			request.destination = destination
+			request.source = source
+			let directions = MKDirections(request: request)
+			directions.calculate { [self](response, error) in
+				if error == nil {
+					if let  response = response {
+						//necessario para limpar qualquer overlay
+						mapView.removeOverlays(mapView.overlays)
+						
+						let route = response.routes.first!
+						//metodos disponiveis com route
+						print("name:\(route.name)")
+						print("distancia:\(route.distance)")
+						print("name:\(route.expectedTravelTime)")
+						
+						//adicionado overlay
+						//abouveRoads pinta em cima da rua
+						mapView.addOverlay(route.polyline, level: .aboveRoads)
+						
+						//aqui estou removendo todas minhas place anotation
+						//assim so tenho a localizacao do usuario
+						var annotations = mapView.annotations.filter({!($0 is PlaceAnnotation)})
+						//dessa forma aqui so vou ter localizacao do usuario
+						//mais a selecinado em selectedAnotation
+						annotations.append(selectedAnotation!)
+						mapView.showAnnotations(annotations, animated: true)
+						
+					}
+					
+				}else {
+					alertLocation(.error)
+					print(error)
+				}
+			}
+	
+		}
+		
 	}
 	
 	
@@ -77,6 +169,32 @@ class MapViewController: UIViewController {
 	@IBAction func showSearchBar(_ sender: UIBarButtonItem) {
 		searchBar.resignFirstResponder()
 		searchBar.isHidden = !searchBar.isHidden //forma de fazer um toogle
+	}
+	
+	
+	func alertLocation(_ type:MapMessageAlert)  {
+		let title = type  == .authorizationWarning ? "Warning" : "Error"
+		let message = type == .authorizationWarning ? "To use this feature I need access to the location" : "Could not trace the route"
+		
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+		alert.addAction(cancel)
+		
+		if type == .authorizationWarning {
+			let confirm = UIAlertAction(title: "Go settings", style: .default) {(action) in
+				//pego o caminho do setings
+				if	let appSetings = URL(string: UIApplicationOpenNotificationSettingsURLString){
+					
+					//pego meu aplicativo com shared e com metodo open
+					//envio para setings
+					UIApplication.shared.open(appSetings,options: [:],completionHandler: nil)
+				}
+				
+			}
+			alert.addAction(confirm)
+		}
+		
+		self.present(alert, animated: true, completion: nil)
 	}
 	
 }
@@ -152,8 +270,57 @@ extension MapViewController: UISearchBarDelegate {
 				print(error?.localizedDescription)
 			}
 		}
+	}
+	
+	//quando tocar em uma annotation
+	func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+		//3d mapa
+		let camera = MKMapCamera()
+		//angulo da camera
+		camera.centerCoordinate = mapView.centerCoordinate
+		camera.pitch = 60
+		camera.altitude = 500
+		mapView.setCamera(camera, animated: true)
 		
+		selectedAnotation = view.annotation as! PlaceAnnotation
+		labName.text = selectedAnotation?.title
+		labAddress.text = selectedAnotation?.address
+		vwInfo.isHidden = false
+	}
+	
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		//criando overlay
+		if overlay is MKPolyline {
+			let renderer = MKPolylineRenderer(overlay: overlay)
+			renderer.strokeColor = UIColor(named: "main")?.withAlphaComponent(0.8)
+			renderer.lineWidth = 5.0
+			return renderer
+		}
+		return MKOverlayRenderer(overlay: overlay)
+	}
+	
+}
+
+// MARK: - CLLocationManagerDelegate
+extension MapViewController:CLLocationManagerDelegate {
+	//aqui literalmente vou saber quando autorizacao foi alterada
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		switch status {
+		case .authorizedWhenInUse,.authorizedAlways:
+			mapView.showsUserLocation = true
+			mapView.addSubview(buttonLocation)
+			locationManager.startUpdatingLocation()
+		default:
+			break
+			
+		}
 		
 	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		//vai printar a ultima localizacao do app
+		print(locations.last)
+	}
+	
 	
 }
